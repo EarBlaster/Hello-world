ce1c74518095e8b5c2237a84589aa34cbff05ce1
EarBlaster <135775214+EarBlaster@users.noreply.github.com>
2023-06-06 22:26:22 -0500

    Update hello-world.py
    added description of how you can commit from command line

--- hello-world.py
+++ hello-world.py
@@ -1,1 +1,25 @@
 print("hello")
+
+##Absolutely, here's a typical workflow using GitHub, VS Code, and the command line on a Mac:
+
+##1. **Create a new repository on GitHub**: Log in to your GitHub account and create a new repository. You can initialize it with a README, .gitignore, and a License if you want.
+
+##2. **Clone the repository to your local machine**: Open Terminal (or the integrated terminal in VS Code using the shortcut `Ctrl + ` backtick key) and navigate to the location where you want to store your project locally using the `cd` (change directory) command. Then, clone your GitHub repository with the command `git clone https://github.com/username/repository.git`, replacing "username" with your GitHub username and "repository" with the name of your GitHub repository. This will create a copy of your repository on your local machine.
+
+##3. **Open the repository in VS Code**: You can do this by typing `code .` in the terminal while you're inside the directory of the cloned repository. This will open the current directory in VS Code.
+
+##4. **Create or edit files**: In VS Code, you can now create new files or edit existing ones. 
+
+##5. **Stage your changes**: Once you've made some changes, go back to your terminal and add these changes to the staging area with `git add .` This tells Git that you want to include the updates to these files in the next commit. 
+
+##6. **Commit your changes**: Create a new commit which includes the changes you've just staged. You can do this with the command `git commit -m "Your commit message"`. Replace "Your commit message" with a brief description of the changes you made.
+
+##7. **Push your changes to GitHub**: You can now push the commit to your repository on GitHub with the command `git push origin main` (or `git push origin master` if your default branch is called "master").
+
+##8. **Pull latest changes**: If there are other people also working on this repository, or if you've made changes to the repository on GitHub directly, you'll want to pull these changes down to your local machine regularly with `git pull origin main` (or `git pull origin master`).
+
+##9. **Repeat**: Repeat this process of making changes, staging them, committing them, and pushing them to GitHub as many times as needed.
+
+##If you're working on a large feature or update, you might want to do this on a separate branch and then merge your changes back to the main branch using a pull request on GitHub. You can create a new branch with `git checkout -b branchname` and switch between branches with `git checkout branchname`. 
+
+##Remember to always pull the latest changes before you start working, especially if you're collaborating with others, to minimize the chance of conflicts.
